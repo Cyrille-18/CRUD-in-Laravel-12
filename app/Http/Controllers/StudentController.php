d611351c004afb8667fc4e0b991f3efcfeb8f153
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-04 14:04:34 +0000

    Feat: Implements Students Creation
    students data are stored in database after submitinf the form

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
@@ -13,8 +14,26 @@ class StudentController extends Controller
     }
 
     // fonction qui va stocker le student dans la base de données
+    // Stocke les données du formulaire dans la base de données
     public function store(Request $request)
     {
-        dd($request->all());
+        // Valider les données
+        $request->validate([
+            'nom' => 'required|string|max:255',
+            'prenom' => 'required|string|max:255',
+            'age' => 'required|integer|min:1',
+            'telephone' => 'required|string|max:20',
+        ]);
+
+        // Insérer dans la base de données
+        Student::create([
+            'nom' => $request->nom,
+            'prenom' => $request->prenom,
+            'age' => $request->age,
+            'telephone' => $request->telephone,
+        ]);
+
+        // Rediriger avec un message de succès
+        return redirect()->back()->with('success', 'Étudiant ajouté avec succès !');
     }
 }
--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -5,28 +5,34 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/student.css') }}">
     <title>Document</title>
+
 </head>
 <body>
-    <form  method="POST" action="{{ route('students.store') }}">
-        @csrf
-        <div>
-            <label id="name">Nom:</label>
-            <input type="text" name="name" id="name">
-        </div>
-        <div>
-            <label id="prenom">Prenom</label>
-            <input type="text" name="prenom" id="prenom">
-        </div>
-        <div>
-            <label id="age">Age</label>
-            <input type="number" name="age" id="age">
-        </div>
-        <div>
-            <label id="telephone">Telephone</label>
-            <input type="text" id="telephone" name="telephone">
-        </div>
-        <button type="submit">Soumettre</button>
-    </form>
+
+<form method="POST" action="{{ route('students.store') }}">
+    @csrf
+    @if(session('success'))
+        <p style="color: green;">{{ session('success') }}</p>
+    @endif
+    <div>
+        <label for="nom">Nom:</label>
+        <input type="text" name="nom" id="nom" required>
+    </div>
+    <div>
+        <label for="prenom">Prénom:</label>
+        <input type="text" name="prenom" id="prenom" required>
+    </div>
+    <div>
+        <label for="age">Âge:</label>
+        <input type="number" name="age" id="age" required>
+    </div>
+    <div>
+        <label for="telephone">Téléphone:</label>
+        <input type="text" name="telephone" id="telephone" required>
+    </div>
+    <button type="submit">Soumettre</button>
+</form>
 </body>
 </html>
