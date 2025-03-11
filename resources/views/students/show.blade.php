eed3ee85cda502bfcf0f4954f42319ad5f887174
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-11 16:30:57 +0000

    Feat: create view for testing show methods

---
+++ resources/views/students/show.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des étudiants</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <h1>Liste des étudiants</h1>
+    </div>
+
+    <div class="table-container">
+       <h1>Vu student show</h1>
+    </div>
+</div>
+</body>
+</html>
+<?php
