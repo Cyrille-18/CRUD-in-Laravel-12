e03e75bfcb5ce1cbecc4534eef3fdddbb6bb3e3e
Cyrille-18 <cyrillepio@hotmail.com>
2025-03-11 16:29:46 +0000

    Feat: add route for displaying show view

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,16 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
-
+Route::get('/', [StudentController::class, 'index'])->name('students.index');
 
 Route::get('/students/create',[StudentController::class, 'create'])->name('students.create');
 
 
 Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+
+Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
+
